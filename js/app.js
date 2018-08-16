e2fdb9ecbb2e288f45ee2fc0090d17d71618b326
Alabi Olasunkanmi <alabiolasunkanmi@outlook.com>
2018-08-16 12:02:34 +0100

    Added strict mode to javaScript, added main tag and viewpoint to html. Also used repeat() function to control the stars instead of listing all the stars out

--- js/app.js
+++ js/app.js
@@ -2,202 +2,198 @@
  * Create a list that holds all of your cards
  */
 
-// Array of card icons
-let icons = ["diamond", "bomb", "paper-plane-o", "anchor", "bolt", "cube", "leaf", "bicycle"];
-let cardIcons = icons.concat(icons);
+// Strict Mode
+(function () {
+    'use strict';
 
-// shuffle card Icons
-let shuffledIcons = shuffle(cardIcons);
-// add the cards to the page
-function createCard() {
-    for (let i = 0; i < 16; i++) {
-        let card = document.createElement('li');
-        card.classList.add('card');
-        card.innerHTML = "<i class= 'fa fa-" + shuffledIcons[i] + "'></i>";
-        document.querySelector('.deck').appendChild(card);
+    // Array of card icons
+    let icons = ["diamond", "bomb", "paper-plane-o", "anchor", "bolt", "cube", "leaf", "bicycle"];
+    let cardIcons = icons.concat(icons);
+
+    // shuffle card Icons
+    let shuffledIcons = shuffle(cardIcons);
+    // add the cards to the page
+    function createCard() {
+        for (let i = 0; i < 16; i++) {
+            let card = document.createElement('li');
+            card.classList.add('card');
+            card.innerHTML = "<i class= 'fa fa-" + shuffledIcons[i] + "'></i>";
+            document.querySelector('.deck').appendChild(card);
+        }
     }
-}
-createCard();
-
-//TODO: to start game
-function startGame() {
-    shuffle(cardIcons);
-    document.querySelector('.deck').innerHTML = "";
     createCard();
-    moveCount = 0;
-    matchedCard = 0;
-    minutes = 0;
-    seconds = 0;
-    firstClick = false;
-    document.querySelector('.timer').innerHTML = minutes + "mins " + seconds + "secs";
-    cardList = [];
-    currentStars = `<li><i class='fa fa-star'></i></li><li><i class="fa fa-star"></i></li> <li> <i class="fa fa-star"></i></li>`;
-    document.querySelector('.stars').innerHTML = currentStars;
-    document.querySelector('.moves').innerHTML = moveCount;
-    clearInterval(timerInterval);
-}
-//TODO: to clear and restart game
-document.querySelector('.restart').addEventListener('click', startGame);
 
-//TODO: function to stop the timer and launch Modal
-function endGame() {
-    if (matchedCard === 8) {
-        stopTime();
-        launchModal();
+    //TODO: to start game
+    function startGame() {
+        shuffle(cardIcons);
+        document.querySelector('.deck').innerHTML = "";
+        createCard();
+        moveCount = 0;
+        matchedCard = 0;
+        minutes = 0;
+        seconds = 0;
+        firstClick = false;
+        document.querySelector('.timer').innerHTML = minutes + "mins " + seconds + "secs";
+        cardList = [];
+        let currentStars = `<li><i class='fa fa-star'></i></li>`;
+        document.querySelector('.stars').innerHTML = currentStars.repeat(3);
+        document.querySelector('.moves').innerHTML = moveCount;
+        clearInterval(timerInterval);
     }
-}
+    //TODO: to clear and restart game
+    document.querySelector('.restart').addEventListener('click', startGame);
 
-// variables used
-let cardList = [];
-let moveCount = 0;
-let matchedCard = 0;
-let timerInterval;
-let firstClick = false;
-let minutes = 0;
-let seconds = 0;
-// rating stars
-let currentStars = `<li><i class='fa fa-star'></i></li><li>
-<i class="fa fa-star"></i>
-</li>
-<li>
-<i class="fa fa-star"></i>
-</li>`;
-document.querySelector('.stars').innerHTML = currentStars;
+    //TODO: function to stop the timer and launch Modal
+    function endGame() {
+        if (matchedCard === 8) {
+            stopTime();
+            launchModal();
+        }
+    }
 
-// Card click logic
-document.querySelector('.deck').addEventListener('click', function (e) {
-    if (e.target.nodeName == 'LI') {
-        // target the card been clicked
-        e.target.classList.add('show');
-        e.target.classList.add('open');
-        e.target.classList.add('click-none');
-        // pass clicked cards into cardList array
-        cardList.push(e.target);
-        startTime();
+    // variables used
+    let cardList = [];
+    let moveCount = 0;
+    let matchedCard = 0;
+    let timerInterval;
+    let firstClick = false;
+    let minutes = 0;
+    let seconds = 0;
+    // rating stars
+    let currentStars = `<li><i class='fa fa-star'></i></li>`;
+    document.querySelector('.stars').innerHTML = currentStars.repeat(3);;
 
-        // I dentify two cards has been clicked
-        if (cardList.length === 2) {
-            let cardOne = cardList[0].firstElementChild.classList[1];
-            let cardTwo = cardList[1].firstElementChild.classList[1];
-            moveCount++;
-            countMoves();
-            starRating();
+    // Card click logic
+    document.querySelector('.deck').addEventListener('click', function (e) {
+        if (e.target.nodeName == 'LI') {
+            // target the card been clicked
+            e.target.classList.add('show');
+            e.target.classList.add('open');
+            e.target.classList.add('click-none');
+            // pass clicked cards into cardList array
+            cardList.push(e.target);
+            startTime();
 
-            // check if clicked cards' icons match
-            if (cardOne === cardTwo) {
-                // add class match if clicked cards matches and remove open and show classes
-                cardList[0].classList.add('match');
-                cardList[1].classList.add('match');
-                cardList[0].classList.remove('open', 'show');
-                cardList[1].classList.remove('open', 'show');
-                // counts number of matches
-                matchedCard++;
-                cardList = [];
-                endGame();
-            } else {
-                // remove class open and show if cards don't match and add unmatched class
-                cardList[0].classList.remove('open', 'show', 'match');
-                cardList[1].classList.remove('open', 'show', 'match');
-                cardList[1].classList.add('unmatched');
-                cardList[0].classList.add('unmatched');
-                // sets timeout so the code can rerun on another click
-                setTimeout(function () {
-                    cardList[0].classList.remove("show", "open", 'click-none', "unmatched");
-                    cardList[1].classList.remove("show", "open", 'click-none', "unmatched");
+            // I dentify two cards has been clicked
+            if (cardList.length === 2) {
+                let cardOne = cardList[0].firstElementChild.classList[1];
+                let cardTwo = cardList[1].firstElementChild.classList[1];
+                moveCount++;
+                countMoves();
+                starRating();
+
+                // check if clicked cards' icons match
+                if (cardOne === cardTwo) {
+                    // add class match if clicked cards matches and remove open and show classes
+                    cardList[0].classList.add('match');
+                    cardList[1].classList.add('match');
+                    cardList[0].classList.remove('open', 'show');
+                    cardList[1].classList.remove('open', 'show');
+                    // counts number of matches
+                    matchedCard++;
                     cardList = [];
-                }, 350);
+                    endGame();
+                } else {
+                    // remove class open and show if cards don't match and add unmatched class
+                    cardList[0].classList.remove('open', 'show', 'match');
+                    cardList[1].classList.remove('open', 'show', 'match');
+                    cardList[1].classList.add('unmatched');
+                    cardList[0].classList.add('unmatched');
+                    // sets timeout so the code can rerun on another click
+                    setTimeout(function () {
+                        cardList[0].classList.remove("show", "open", 'click-none', "unmatched");
+                        cardList[1].classList.remove("show", "open", 'click-none', "unmatched");
+                        cardList = [];
+                    }, 350);
+                }
+
             }
+        }
+    })
+
+    // add counts to score board
+    function countMoves() {
+        document.querySelector('.moves').innerHTML = moveCount;
+    }
+
+    // Star Rating Function
+    function starRating() {
+        // removes a star at different move count
+        if (moveCount === 12) {
+            document.querySelector('.stars').innerHTML = currentStars.repeat(2);
+
+        } else if (moveCount === 18) {
+            document.querySelector('.stars').innerHTML = currentStars;
 
         }
     }
-})
 
-// add counts to score board
-function countMoves() {
-    document.querySelector('.moves').innerHTML = moveCount;
-}
-
-// Star Rating Function
-function starRating() {
-    // removes a star at different move count
-    if (moveCount === 12) {
-        currentStars = `<li><i class='fa fa-star'></i></li><li>
-        <i class="fa fa-star"></i>
-        </li>`;
-        document.querySelector('.stars').innerHTML = currentStars;
-
-    } else if (moveCount === 18) {
-        currentStars = `<li><i class='fa fa-star'></i></li>`;
-        document.querySelector('.stars').innerHTML = currentStars;
-
+    // Function to start timer
+    function startTime() {
+        if (!firstClick) {
+            firstClick = true;
+            timerInterval = setInterval(updateTime, 1000);
+        }
     }
-}
-
-// Function to start timer
-function startTime() {
-    if (!firstClick) {
-        firstClick = true;
-        timerInterval = setInterval(updateTime, 1000);
+    // function to update Timer
+    function updateTime() {
+        seconds++;
+        if (seconds === 60) {
+            minutes++;
+            seconds = 0;
+        }
+        document.querySelector('.timer').innerHTML = minutes + "mins " + seconds + "secs";
     }
-}
-// function to update Timer
-function updateTime() {
-    seconds++;
-    if (seconds === 60) {
-        minutes++;
-        seconds = 0;
+    // function to stop timer when the game is won
+    function stopTime() {
+        clearInterval(timerInterval);
     }
-    document.querySelector('.timer').innerHTML = minutes + "mins " + seconds + "secs";
-}
-// function to stop timer when the game is won
-function stopTime() {
-    clearInterval(timerInterval);
-}
 
-// function for launching congratulatory modal
-function launchModal() {
-    // Time for modal to launch after last match as beem made
-    setTimeout(function () {
-        document.querySelector('.modal').classList.remove('close-modal');
-        // Displays congratulatory message with scores
-        document.querySelector('.modal-text').innerHTML =
-            "You made " + moveCount + " moves in " + document.querySelector('.timer').innerHTML + " and your rating is " + currentStars;
-    }, 2000);
-    // close modal upon click
-    document.querySelector('.close-modal-btn ').addEventListener('click', function () {
+    // function for launching congratulatory modal
+    function launchModal() {
+        // Time for modal to launch after last match as beem made
+        setTimeout(function () {
+            document.querySelector('.modal').classList.remove('close-modal');
+            // Displays congratulatory message with scores
+            document.querySelector('.modal-text').innerHTML =
+                "You made " + moveCount + " moves in " + document.querySelector('.timer').innerHTML + " and your rating is " + currentStars;
+        }, 2000);
+        // close modal upon click
+        document.querySelector('.close-modal-btn ').addEventListener('click', function () {
+            document.querySelector('.modal').classList.add('close-modal');
+        })
+    }
+
+    // TODO: restart the game when clicked and close the modal
+    document.querySelector('.modal-play-again').addEventListener('click', function () {
+        startGame();
         document.querySelector('.modal').classList.add('close-modal');
-    })
-}
-
-// TODO: restart the game when clicked and close the modal
-document.querySelector('.modal-play-again').addEventListener('click', function () {
-    startGame();
-    document.querySelector('.modal').classList.add('close-modal');
-});
+    });
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+    /*
+     * Display the cards on the page
+     *   - shuffle the list of cards using the provided "shuffle" method below
+     *   - loop through each card and create its HTML
+     *   - add each card's HTML to the page
+     */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length,
-        temporaryValue, randomIndex;
+    // Shuffle function from http://stackoverflow.com/a/2450976
+    function shuffle(array) {
+        var currentIndex = array.length,
+            temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
     }
-
-    return array;
-}
+}());
 
 
 
